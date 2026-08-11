c2fb67ba62c6b37b0619dd89dc17e8f67245afef
M-Ilpa <mazenilpa.2007@gmail.com>
2026-08-12 01:26:21 +0300

    fixed the menu appearing from any click

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-const btn = document.getElementById("hidden");
-const mobileNav = document.getElementById("mobile-nav")
+const btn = document.getElementById("nav-icon");
+const mobileNav = document.getElementById("mobile-nav");
+const wholeScreen =document.getElementById("box");
 
-btn.addEventListener("click", function() {
-    if (mobileNav.style.display === "none") {
+btn.addEventListener("click", function(event) {
+    event.stopPropagation();
+    if (mobileNav.style.display !== "flex") {
         mobileNav.style.display = "flex";
+        wholeScreen.style.pointerEvents = "auto";
     }else {
         mobileNav.style.display = "none";
+        wholeScreen.style.pointerEvents = "auto";
     }
     
+});
+wholeScreen.addEventListener("click", function() {
+    if (mobileNav.style.display === "flex") {
+        mobileNav.style.display = "none";
+        wholeScreen.style.pointerEvents = "none";
+    }
 });
